f72ac5f161337862885f4b674dbbb31fe1bf9201
Pavan-172 <71075561+Pavan-172@users.noreply.github.com>
2021-04-11 17:29:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+prompt("enter your Name");
+prompt("enter your Name");
+
+var loveScore=Math.random()*100;
+
+loveScore=Math.floor(loveScore)+1;;
+
+if(loveScore>80){
+alert("Your love score is "+loveScore+"%"+" like Romeo and Juliet")
+}
+else if(loveScore>=30 && loveScore<=80){
+    alert("Your love score is "+loveScore+"%")
+}
+else{
+    alert("Your love score is "+loveScore+"%"+" like oil and water")
+}
+
